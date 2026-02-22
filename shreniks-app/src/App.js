a66801a72c3c18499c98ef0217cb8877c1bd3dca
shrenik <shrenik@gmail.com>
2026-02-22 10:46:57 +0530

    initial commit and pull the changes

---
+++ shreniks-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-red-900 text-2xl">Welcome to learn with live coding challenge</h1>
+    </div>
+  );
+}
+
+export default App;
